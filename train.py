47ba066ceafa6a841ef057582a4ef43f5d4a035e
Steffy-zxf <xuefegzh@amazon.com>
2022-05-16 15:35:41 +0800

    add weight decay and warmup

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import pandas as pd
 import torch
 import torch.distributed as dist
 import transformers
+from sklearn.metrics import classification_report
 from torch.nn.parallel import DistributedDataParallel
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
@@ -25,14 +26,14 @@ from utils import write_vocab
 # yapf: disable
 parser = argparse.ArgumentParser(__doc__)
 parser.add_argument("--epochs", type=int, default=200, help="Number of epoches for training.")
-parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight decay if we apply some.")
-parser.add_argument("--warmup_prop", default=0.0, type=float, help="Linear warmup proption over the training process.")
+parser.add_argument("--weight_decay", default=0.01, type=float, help="Weight decay if we apply some.")
+parser.add_argument("--warmup_prop", default=0.1, type=float, help="Linear warmup proption over the training process.")
 parser.add_argument('--device', choices=['cpu', 'cuda'], default="cuda", help="Select which device to train model, defaults to gpu.")
 parser.add_argument("--local_rank", type=int, default=0)
-parser.add_argument("--lr", type=float, default=5e-6, help="Learning rate used to train.")
+parser.add_argument("--lr", type=float, default=5e-5, help="Learning rate used to train.")
 parser.add_argument("--data_dir", type=str, default='data/', help="Directory to data.")
 parser.add_argument("--save_dir", type=str, default='checkpoints/', help="Directory to save model checkpoint")
-parser.add_argument("--batch_size", type=int, default=16, help="Total examples' number of a batch for training.")
+parser.add_argument("--batch_size", type=int, default=32, help="Total examples' number of a batch for training.")
 parser.add_argument("--init_from_ckpt", type=str, default=None, help="The path of checkpoint to be loaded.")
 parser.add_argument('--seed', type=int, default=1000, help='random seed (default: 1000)')
 args = parser.parse_args()
@@ -64,10 +65,12 @@ def collate_batch(batch, tokenizer, label_dict):
     # return texts, seq_lens, labels
 
 
-def train(device, dataloader, model, optimizer, scheduler, criterion, epoch):
+def train(device, dataloader, model, optimizer, scheduler, criterion, epoch, inv_label):
     model.train()
+    all_preds, all_labels = [], []
     total_acc, total_count = 0, 0
     log_interval = 10
+    label_names = list(inv_label.values())
 
     for idx, (input_ids, token_type_ids, attention_mask, labels) in enumerate(dataloader):
         input_ids = input_ids.to(device)
@@ -76,18 +79,27 @@ def train(device, dataloader, model, optimizer, scheduler, criterion, epoch):
         labels = labels.to(device)
         optimizer.zero_grad()
         logits = model(input_ids, attention_mask, token_type_ids)["logits"]
-        probs = torch.softmax(logits, dim=-1)
-        loss = criterion(probs, labels)
+        preds = torch.argmax(logits, dim=-1)
+        preds = preds.numpy().tolist()
+        preds = [inv_label[index] for index in preds]
+        true_labels = [inv_label[index] for index in labels.numpy().tolist()]
+        all_preds.extend(preds)
+        all_labels.extend(true_labels)
+
+        loss = criterion(logits, labels)
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.1)
         optimizer.step()
         scheduler.step()
-        total_acc += (probs.argmax(1) == labels).sum().item()
+
+        total_acc += (logits.argmax(1) == labels).sum().item()
         total_count += labels.size(0)
         if idx % log_interval == 0 and idx > 0:
             print("| epoch {:3d} | {:5d}/{:5d} batches "
                   "| accuracy {:8.3f}".format(epoch, idx, len(dataloader), total_acc / total_count))
+            t = classification_report(all_labels, all_preds, target_names=label_names)
+            print(t)
             total_acc, total_count = 0, 0
+            all_preds, all_labels = [], []
 
 
 def evaluate(
@@ -130,6 +142,7 @@ if __name__ == "__main__":
     tags_file = os.path.join(args.data_dir, "tags.txt")
     with open(tags_file, "r", encoding="utf8") as f:
         label_dict = json.load(f)
+        inv_label = {value: key for key, value in label_dict.items()}
 
     # vocab = build_vocab(texts)
     vocab_file = os.path.join(args.data_dir, "vocab.json")
@@ -154,15 +167,22 @@ if __name__ == "__main__":
     # model = BiLSTM(len(vocab), len(label_dict), padding_idx=vocab["[PAD]"])
     model = model.to(device)
     model = torch.nn.DataParallel(model)
-    optimizer = torch.optim.AdamW(lr=args.lr, params=model.parameters(), weight_decay=args.weight_decay)
+    no_decay = ['bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [{
+        'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+        'weight_decay':
+        args.weight_decay
+    }, {
+        'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+        'weight_decay':
+        0.0
+    }]
     total_steps = args.epochs * len(train_loader)
     warmup_steps = total_steps * args.warmup_prop
-    scheduler = get_linear_schedule_with_warmup(
-        optimizer,
-        num_warmup_steps=warmup_steps,  # Default value in run_glue.py
-        num_training_steps=total_steps)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=5e-5)
+    optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=args.lr)
+    scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                num_warmup_steps=warmup_steps,
+                                                num_training_steps=total_steps)
     criterion = torch.nn.CrossEntropyLoss()
 
     if not os.path.exists(args.save_dir):
